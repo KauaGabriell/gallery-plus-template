9fab994f975bfb9811bac90216a956ec047f64a8
KauaGabriell <kauagabriell64@gmail.com>
2026-08-12 13:30:25 -0300

    feat(upload): integrate single file input with form state

--- src/components/InputSingleFile.tsx
+++ src/components/InputSingleFile.tsx
@@ -1,8 +1,10 @@
+import React from "react";
+import { useWatch } from "react-hook-form";
 import { tv, type VariantProps } from "tailwind-variants";
 import ImageIcon from "../assets/icons/image.svg?react";
 import UploadIcon from "../assets/icons/upload-file.svg?react";
 import Icon from "./Icon";
-import Text, { textVariants } from "./Text";
+import Text from "./Text";
 
 export const inputSingleFileVariants = tv({
 	base: "flex flex-col items-center justify-center w-full border border-solid border-border-primary group-hover:border-border-active rounded-lg gap-1 transition",
@@ -17,43 +19,65 @@ export const inputSingleFileVariants = tv({
 interface SingleFileProps
 	extends VariantProps<typeof inputSingleFileVariants>,
 		Omit<React.ComponentProps<"input">, "size"> {
+	form: UseFormReturn;
 	error?: React.ReactNode;
 }
 
-export function InputSingleFile({ size, error }: SingleFileProps) {
+export function InputSingleFile({
+	size,
+	error,
+	form,
+	...props
+}: SingleFileProps) {
+	const formValues = useWatch({ control: form.control });
+	const name = props.name || "";
+	const formFile: File = React.useMemo(
+		() => formValues[name]?.[0],
+		[formValues, name],
+	);
+
+	function handleRemoveFile() {
+		form.setValue(name, undefined);
+	}
+
 	return (
 		<div>
-			<div className="w-full relative group cursor-pointer">
-				<input
-					type="file"
-					className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer"
-				/>
-				<div className={inputSingleFileVariants({ size })}>
-					<Icon svg={UploadIcon} className="fill-accent-span w-8 h-8" />
-					<Text variant="label-medium" className="text-placeholder">
-						Arraste o Arquivo aqui <br /> ou clique para selecionar
-					</Text>
+			{!formFile ? (
+				<div className="w-full relative group cursor-pointer">
+					<input
+						type="file"
+						className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer"
+						{...props}
+					/>
+					<div className={inputSingleFileVariants({ size })}>
+						<Icon svg={UploadIcon} className="fill-accent-span w-8 h-8" />
+						<Text variant="label-medium" className="text-placeholder">
+							Arraste o Arquivo aqui <br /> ou clique para selecionar
+						</Text>
+					</div>
+					{error && (
+						<Text variant="label-small" className="text-accent-red">
+							{error}
+						</Text>
+					)}
 				</div>
-				{error && (
-					<Text variant="label-small" className="text-accent-red">
-						{error}
-					</Text>
-				)}
-			</div>
-			<div className="flex items-center gap-3 border-2 border-border-primary rounded p-3 mt-5">
-				<Icon svg={ImageIcon} className="w-6 h-6 fill-white" />
-				<div className="flex flex-col w-full items-start">
-					<Text variant="label-medium" className="text-placeholder">
-						nomeArquivo.png
-					</Text>
-					<button
-						type="button"
-						className="text-accent-red hover:underline cursor-pointer"
-					>
-						Remover
-					</button>
+			) : (
+				<div className="flex items-center gap-3 border-2 border-border-primary rounded p-3 mt-5">
+					<Icon svg={ImageIcon} className="w-6 h-6 fill-white" />
+					<div className="flex flex-col w-full items-start">
+						<Text variant="label-medium" className="text-placeholder">
+							{formFile.name}
+						</Text>
+						<button
+							type="button"
+							className="text-accent-red hover:underline cursor-pointer"
+							onClick={handleRemoveFile}
+						>
+							Remover
+						</button>
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
