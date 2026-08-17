d3df0b897e4959603927c062d3a29b8f6257829e
KauaGabriell <kauagabriell64@gmail.com>
2026-08-17 09:45:24 -0300

    feat(layout): add MainContent component to wrap Outlet

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import { MainHeader } from "../components/Header";
+import { MainContent } from "../components/MainContent";
 
 export function Layout() {
 	return (
 		<div className="py-6">
 			<MainHeader />
-			<Outlet />
+			<MainContent>
+				<Outlet />
+			</MainContent>
 		</div>
 	);
 }
