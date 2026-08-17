6e5fa2ec1dbb8b8dee7f3f5af459fa5157139a07
KauaGabriell <kauagabriell64@gmail.com>
2026-08-17 10:40:40 -0300

    feat(photos): add photo widget component

---
+++ src/contexts/photos/components/PhotoWidget.tsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router";
+import imgTest from "../../../assets/images/enchanted-forest-fantasy-background.jpg";
+import Badge from "../../../components/Badge";
+import { buttonTextVariants, buttonVariants } from "../../../components/Button";
+import { ImagePreview } from "../../../components/ImagePreview";
+import Skeleton from "../../../components/Skeleton";
+import Text from "../../../components/Text";
+import type { Photo } from "../models/photo";
+
+interface PhotoWidgetProps {
+	photo: Photo;
+	loading?: boolean;
+}
+
+export function PhotoWidget({ photo, loading }: PhotoWidgetProps) {
+	return (
+		<div className="flex flex-col gap-4">
+			{!loading ? (
+				<ImagePreview
+					imgSource={imgTest}
+					title={photo.title}
+					className="w-[10.875rem] h-[10.875rem] rounded-lg"
+				/>
+			) : (
+				<Skeleton className="max-w-[10.875rem] h-[10.875rem] rounded-lg" />
+			)}
+
+			<div className="flex flex-col gap-2">
+				{!loading ? (
+					<Text variant="paragraph-large" className="truncate">
+						{photo.title}
+					</Text>
+				) : (
+					<Skeleton className="w-full h-6" />
+				)}
+				<div className="flex felx-col gap-1 min-h-5">
+					{!loading ? (
+						<>
+							{photo.album.slice(0, 1).map((album) => (
+								<Badge className="truncate" size="xs" key={album.id}>
+									{album.title}
+								</Badge>
+							))}
+							{photo.album.length > 1 && (
+								<Badge size="xs">+{photo.album.length - 1}</Badge>
+							)}
+						</>
+					) : (
+						Array.from({ length: 2 }).map((_, index) => (
+							<Skeleton
+								className="w-full h-4 rounded-sm"
+								key={`album-loadind-${
+									// biome-ignore lint/suspicious/noArrayIndexKey: <Key>
+									index
+								}`}
+							/>
+						))
+					)}
+				</div>
+			</div>
+			{!loading ? (
+				<Link
+					to={`/fotos/${photo.id}`}
+					className={buttonVariants({
+						variant: "secondary",
+						className: "px-2 py-2",
+					})}
+				>
+					<Text
+						variant="label-medium"
+						className={buttonTextVariants({ variant: "secondary", size: "sm" })}
+					>
+						Detalhes da Imagem
+					</Text>
+				</Link>
+			) : (
+				<Skeleton className="w-full h-10" />
+			)}
+		</div>
+	);
+}
