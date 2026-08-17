09eaeed8173dd9e1dabd374c9182009138084cce
KauaGabriell <kauagabriell64@gmail.com>
2026-08-17 10:40:40 -0300

    feat(home): display photo widgets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,45 @@
-import Text from "../components/Text";
+import Container from "../components/Container";
+import { PhotoWidget } from "../contexts/photos/components/PhotoWidget";
 
 export function Home() {
-	return <Text variant="heading-large">Página Home</Text>;
+	return (
+		<Container>
+			<div className="grid grid-cols-5 gap-9">
+				<PhotoWidget
+					photo={{
+						id: "1",
+						title: "Foto",
+						imgId: "enchanted-forest-fantasy-background.jpg",
+						album: [{ id: "2", title: "Natureza" }],
+					}}
+				/>
+				<PhotoWidget
+					loading
+					photo={{
+						id: "1",
+						title: "Foto",
+						imgId: "enchanted-forest-fantasy-background.jpg",
+						album: [{ id: "2", title: "Natureza" }],
+					}}
+				/>
+				<PhotoWidget
+					loading
+					photo={{
+						id: "1",
+						title: "Foto",
+						imgId: "enchanted-forest-fantasy-background.jpg",
+						album: [{ id: "2", title: "Natureza" }],
+					}}
+				/>
+				<PhotoWidget
+					photo={{
+						id: "1",
+						title: "Foto",
+						imgId: "enchanted-forest-fantasy-background.jpg",
+						album: [{ id: "2", title: "Natureza" }],
+					}}
+				/>
+			</div>
+		</Container>
+	);
 }
