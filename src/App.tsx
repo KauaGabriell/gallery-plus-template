6278fb1b867479fa7d5cedf30758b383d2dd82c2
KauaGabriell <kauagabriell64@gmail.com>
2026-08-18 11:07:57 -0300

    feat(api): add query client foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Components } from "./pages/Components";
 import { Home } from "./pages/Home";
 import { Layout } from "./pages/Layout";
 import { PhotoDetails } from "./pages/PhotoDetails";
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="/components" element={<Components />} />
-					<Route path="/fotos/:id" element={<PhotoDetails />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="/components" element={<Components />} />
+						<Route path="/fotos/:id" element={<PhotoDetails />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
